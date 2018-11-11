9aff994b9db4aa0b5a36dc5742e63a75af7d6785
emersonklim <44900926+emersonklim@users.noreply.github.com>
2018-11-11 12:45:06 -0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,8 +1,6 @@
 <?php
-
 $dbuser = $_ENV['MYSQL_USER'];
 $dbpass = $_ENV['MYSQL_PASS'];
-
 try {
     $pdo = new PDO("mysql:host=mysql;dbname=blog", $dbuser, $dbpass);
     $statement = $pdo->prepare("SELECT * FROM posts");
@@ -13,6 +11,18 @@ try {
     echo "<ul>";
     foreach ($posts as $post ) {
         echo "<li>".$post->title."</li>";
+    }
+    echo "</ul>";
+
+$pdo = new PDO("mysql:host=mysql;dbname=blog", $dbuser, $dbpass);
+    $statement = $pdo->prepare("SELECT * FROM posts where id=3");
+    $statement->execute();
+    $ultimos = $statement->fetchAll(PDO::FETCH_OBJ);
+    
+    echo "<h2>Ultimo Post</h2>";
+    echo "<ul>";
+    foreach ($ultimos as $post ) {
+        echo "<li><b>Ultimo</b>".$ultimos->title."</li>";
     }
     echo "</ul>";
 
